7431c8a41fc16f5add018904a74dbd8b2154bda8
Wertops <haella@live.unc.edu>
2021-12-04 19:28:07 -0500

    fixed single user id

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").all();
-	res.status(200).json(stmt);
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
+	res.json(stmt);
+	res.status(200);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
